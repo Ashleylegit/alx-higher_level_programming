e3f3910bda389be7eef8a366a4135f357c656b28
Ashleylegit <99674466+Ashleylegit@users.noreply.github.com>
2022-09-27 11:28:15 -0600

    input and output

--- 0x0B-python-input_output/8-load_from_json_file.py
+++
@@ -1,8 +0,0 @@
-"""Defines a JSON file-reading function."""
-import json
-
-
-def load_from_json_file(filename):
-    """Create a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
