0951645c01461af293e67d759a123af466bb7303
Ashleylegit <99674466+Ashleylegit@users.noreply.github.com>
2023-11-28 23:11:10 -0500

    0x00-python-hello_world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - Checks if a linked list has a cycle.
+ * @list: A pointer to the head of the linked list.
+ *
+ * Return: 1 if the linked list has a cycle, 0 otherwise.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+	
+	if (list == NULL || list->next == NULL)
+		return 0;
+	
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
+}
